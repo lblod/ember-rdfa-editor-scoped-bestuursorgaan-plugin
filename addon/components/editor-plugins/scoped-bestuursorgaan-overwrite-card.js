4c8d10b6f2447e518156d68937a3da3a270f1847
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-10-19 11:15:27 +0200

    Plugin needs to take into account when no text changes occur.
    This should be one day obsolete code path

--- addon/components/editor-plugins/scoped-bestuursorgaan-overwrite-card.js
+++ addon/components/editor-plugins/scoped-bestuursorgaan-overwrite-card.js
@@ -1,4 +1,5 @@
 import Component from '@ember/component';
+import EmberObject from '@ember/object';
 import layout from '../../templates/components/editor-plugins/scoped-bestuursorgaan-overwrite-card';
 import InsertResourceRelationCardMixin from '@lblod/ember-generic-model-plugin-utils/mixins/insert-resource-relation-card-mixin';
 import { inject as service } from '@ember/service';
@@ -8,6 +9,8 @@ export default Component.extend(InsertResourceRelationCardMixin, {
   layout,
   currentSession: service(),
   store: service(),
+
+  card: 'editor-plugins/scoped-bestuursorgaan-overwrite-card',
 
   getBestuursorganen: task(function * (){
     let currentBestuurseenheid = yield this.currentSession.get('group');
@@ -33,15 +36,40 @@ export default Component.extend(InsertResourceRelationCardMixin, {
     return rdfa;
   },
 
+  //if text doesn't change, nothing will be triggered. So we need to keep hint
+  async needsNewHint(data, nodeToReplace){
+    return (await data.get('isTijdsspecialisatieVan.uri')) == nodeToReplace.attributes.resource.value;
+  },
+
    didReceiveAttrs() {
     this._super(...arguments);
     this.get('getBestuursorganen').perform();
    },
 
+  generateCard(domNode){
+    return EmberObject.create({
+      info: {
+        label: 'Voeg het relevante bestuursorgaan toe.',
+        plainValue: this.info.text,
+        location: this.location,
+        domainUri: this.info.domainUri,
+        domNodeToUpdate: domNode,
+        instructiveUri: this.info.instructiveUri,
+        hrId: this.hrId, hintsRegistry: this.hintsRegistry, editor: this.editor
+      },
+      location: this.location,
+      options: { noHighlight: true },
+      card: this.card
+    });
+  },
+
   actions: {
     async refer(data){
-      this.editor.replaceNodeWithHTML(this.info.domNodeToUpdate, await this.buildRdfa(data, this.info.domNodeToUpdate), true);
-      this.get('hintsRegistry').removeHintsAtLocation(this.location, this.get('hrId'), 'editor-plugins/scoped-bestuursorgaan-overwrite-card');
+      let newNodes = this.editor.replaceNodeWithHTML(this.info.domNodeToUpdate, await this.buildRdfa(data, this.info.domNodeToUpdate), true);
+      this.get('hintsRegistry').removeHintsAtLocation(this.location, this.get('hrId'), this.card);
+      if(await this.needsNewHint(data, this.info.domNodeToUpdate)){
+        this.hintsRegistry.addHints(this.hrId, this.card, [ this.generateCard(newNodes[0]) ]);
+      }
     }
   }
 });
